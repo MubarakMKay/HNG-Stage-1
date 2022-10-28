aec80917e410976cf317ca2e19108b4d40dfa551
MubarakMKay <mubi4lyf@gmail.com>
2022-10-28 17:07:39 +0100

    some little changes

--- src/Link.js
+++ src/Link.js
@@ -3,9 +3,7 @@ import "./Styles/Link.css"
 const Link = (props) => {
     return ( 
         <div className="link">
-            <a className="linkButton" href={props.site} id={props.siteId} title={props.siteTitle}>
-                <button>{props.siteDisplay}</button> 
-            </a>
+            <a className="linkButton" href={props.site} id={props.siteId} title={props.siteTitle}>{props.siteDisplay}</a>
         </div>
      );
 }
